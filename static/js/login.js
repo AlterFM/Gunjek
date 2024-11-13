9eabb4ee8039d3793a89bdd2d4ccd42c13332715
AlterFM <yogawindy98@gmail.com>
2024-11-13 10:41:22 +0700

    Upload-KTm

--- static/js/login.js
+++ static/js/login.js
@@ -23,9 +23,23 @@ document.getElementById('show-login').addEventListener('click', function(event) 
     document.getElementById('register-form').style.display = 'none'; // Sembunyikan form daftar
     document.getElementById('login-form').style.display = 'block'; // Tampilkan form login
 });
-// Toggle masuk sebagai user/driver
-function toggleMode() {
-    const modeText = document.getElementById("mode-text");
-    const isChecked = document.getElementById("mode-toggle").checked;
+// // Toggle masuk sebagai user/driver
+// function toggleMode() {
+//     const modeText = document.getElementById("mode-text");
+//     const isChecked = document.getElementById("mode-toggle").checked;
+//     modeText.textContent = isChecked ? "Masuk sebagai Driver" : "Masuk sebagai User";
+// }
+
+// Toggle login form
+function toggleModelogin() {
+    const modeText = document.getElementById("mode-text-login");
+    const isChecked = document.getElementById("mode-toggle-login").checked;
     modeText.textContent = isChecked ? "Masuk sebagai Driver" : "Masuk sebagai User";
+}
+
+// Toggle daftar form
+function toggleModedaftar() {
+    const modeText = document.getElementById("mode-text-register");
+    const isChecked = document.getElementById("mode-toggle-register").checked;
+    modeText.textContent = isChecked ? "Daftar sebagai Driver" : "Daftar sebagai User";
 }
