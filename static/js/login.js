82c8bed568dc6f6245123a6dbd556c1b53208e1d
AlterFM <yogawindy98@gmail.com>
2024-12-31 10:41:54 +0700

    a

--- static/js/login.js
+++ static/js/login.js
@@ -38,10 +38,3 @@ document.getElementById('mode-toggle-register').addEventListener('change', funct
     showForm(this.checked ? 'driverRegister' : 'register');
 });
 
-//logout
-document.getElementById('logout-button').addEventListener('click', function() {
-    // Perform logout actions here, such as clearing session data or redirecting to a login page
-    alert('You have been logged out.');
-    // Example: Redirect to login page
-    window.location.href = '/login';
-});
